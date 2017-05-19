7d2a8eed222a7501a84ea1c3c288b9820c4f0d33
ao1415 <taku31126816@yahoo.co.jp>
2017-05-19 19:54:27 +0900

    定数コンテナ用のアロケータ追加

---
+++ CppLib/Queue.hpp
@@ -0,0 +1,53 @@
+#pragma once
+
+template<typename Type, size_t Size>
+class Queue {
+public:
+
+	Queue() = default;
+	Queue(const Queue& other) {
+
+	}
+	Queue(Queue&& other) {
+
+	}
+
+	const bool empty() const {
+		return start == end;
+	}
+
+	const int size() const {
+		return 0;
+	}
+
+	const Type& front() const {
+
+	}
+
+	void push(const Type& v) {
+
+	}
+	void push(Type&& v) {
+
+	}
+
+	void emplace(Type&& v) {
+
+	}
+
+	void pop() {
+
+	}
+
+	void swap(Queue& other) {
+
+	}
+
+private:
+
+	Type m_data[Size];
+
+	int start = 0;
+	int end = 0;
+
+};
