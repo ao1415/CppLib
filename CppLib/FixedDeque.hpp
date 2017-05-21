2a9b4e0f9c2b34608fa437db173e6e5ae6c26c87
ao1415 <taku31126816@yahoo.co.jp>
2017-05-21 12:54:50 +0900

    FixedDequeの完成

--- CppLib/FixedDeque.hpp
+++ CppLib/FixedDeque.hpp
@@ -6,7 +6,7 @@
 namespace alib {
 
 	/// <summary>
-	/// 
+	/// 要素数が固定のdeque
 	/// </summary>
 	template<typename Type, size_t Size>
 	class FixedDeque {
@@ -26,14 +26,17 @@ namespace alib {
 
 	public:
 
-		void showArray() const {
-			for (size_t i = 0; i < size(); i++) {
-				cerr << i << ":" << m_data[i] << endl;
-			}
-		}
-
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
 		FixedDeque() = default;
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
 		FixedDeque(const FixedDeque& other) = default;
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
 		FixedDeque(FixedDeque&& other) {
 			begin = other.begin;
 			end = other.end;
@@ -41,39 +44,96 @@ namespace alib {
 			m_data = std::move(other.m_data);
 		}
 
+		/// <summary>
+		/// コピー演算子
+		/// </summary>
+		/// <param name="other">Deque</param>
+		/// <returns>自分の参照</returns>
 		FixedDeque& operator=(const FixedDeque& other) = default;
+		/// <summary>
+		/// ムーブ演算子
+		/// </summary>
+		/// <param name="other">Deque</param>
+		/// <returns>自分の参照</returns>
 		FixedDeque& operator=(FixedDeque&& other) = default;
 
+		/// <summary>
+		/// 先頭に要素を追加
+		/// </summary>
+		/// <param name="val">要素</param>
 		void push_front(const Type& val) {
-			element++;
-			begin = (begin - 1) & bit_mask();
+			if (full())
+			{
+				throw std::out_of_range("FixedDeque full before push");
+			}
+			else
+			{
+				element++;
+				begin = (begin - 1) & bit_mask();
 
-			m_data[begin] = val;
+				m_data[begin] = val;
+			}
 		}
+		/// <summary>
+		/// 終端に要素を追加
+		/// </summary>
+		/// <param name="val">要素</param>
 		void push_back(const Type& val) {
-			m_data[end] = val;
+			if (full())
+			{
+				throw std::out_of_range("FixedDeque full back push");
+			}
+			else
+			{
+				m_data[end] = val;
 
-			element++;
-			end = (end + 1) & bit_mask();
+				element++;
+				end = (end + 1) & bit_mask();
+			}
 		}
 
+		/// <summary>
+		/// 先頭に要素を追加
+		/// </summary>
+		/// <param name="val">要素</param>
 		void push_front(Type&& val) {
-			element++;
-			begin = (begin - 1) & bit_mask();
+			if (full())
+			{
+				throw std::out_of_range("FixedDeque full before push");
+			}
+			else
+			{
+				element++;
+				begin = (begin - 1) & bit_mask();
 
-			m_data[begin] = std::move(val);
+				m_data[begin] = std::move(val);
+			}
 		}
+		/// <summary>
+		/// 終端に要素を追加
+		/// </summary>
+		/// <param name="val">要素</param>
 		void push_back(Type&& val) {
-			m_data[end] = std::move(val);
+			if (full())
+			{
+				throw std::out_of_range("FixedDeque full back push");
+			}
+			else
+			{
+				m_data[end] = std::move(val);
 
-			element++;
-			end = (end + 1) & bit_mask();
+				element++;
+				end = (end + 1) & bit_mask();
+			}
 		}
 
+		/// <summary>
+		/// 先頭の要素を削除する
+		/// </summary>
 		void pop_front() {
 			if (empty())
 			{
-				_DEBUG_ERROR("FixedDeque empty before pop");
+				throw std::out_of_range("FixedDeque empty before push");
 			}
 			else
 			{
@@ -81,10 +141,13 @@ namespace alib {
 				begin = (begin + 1) & bit_mask();
 			}
 		}
+		/// <summary>
+		/// 終端の要素を削除する
+		/// </summary>
 		void pop_back() {
 			if (empty())
 			{
-				_DEBUG_ERROR("FixedDeque empty before pop");
+				throw std::out_of_range("FixedDeque empty back push");
 			}
 			else
 			{
@@ -93,20 +156,41 @@ namespace alib {
 			}
 		}
 
+		/// <summary>
+		/// 最大要素数を返す
+		/// </summary>
+		/// <returns>最大要素数</returns>
 		constexpr size_t size() const noexcept {
 			return containerAllocator(Size);
 		}
+		/// <summary>
+		/// 現在の要素数を返す
+		/// </summary>
+		/// <returns>現在の要素数</returns>
 		size_t element_num() const noexcept {
 			return element;
 		}
 
+		/// <summary>
+		/// 追加された要素がないかを返す
+		/// </summary>
+		/// <returns>要素がなければばtrue, それ以外ならfalse</returns>
 		bool empty() const noexcept {
 			return element == 0;
 		}
+		/// <summary>
+		/// 要素がいっぱいかを返す
+		/// </summary>
+		/// <returns>いっぱいならばtrue, それ以外ならfalse</returns>
 		bool full() const noexcept {
 			return element >= size();
 		}
 
+		/// <summary>
+		/// 要素にアクセスする
+		/// </summary>
+		/// <param name="pos">先頭からの番地</param>
+		/// <returns>指定された要素</returns>
 		const Type& at(size_t pos) const {
 			if (pos >= element) throw std::out_of_range("FixedDeque::at");
 
@@ -114,6 +198,11 @@ namespace alib {
 			n &= bit_mask();
 			return m_data[n];
 		}
+		/// <summary>
+		/// 要素にアクセスする
+		/// </summary>
+		/// <param name="pos">先頭からの番地</param>
+		/// <returns>指定された要素</returns>
 		Type& at(size_t pos) {
 			if (pos >= element) throw std::out_of_range("FixedDeque::at");
 
@@ -122,37 +211,71 @@ namespace alib {
 			return m_data[n];
 		}
 
+		/// <summary>
+		/// 要素にアクセスする
+		/// </summary>
+		/// <param name="pos">先頭からの番地</param>
+		/// <returns>指定された要素</returns>
 		const Type& operator[](size_t pos) const {
 			size_t n = begin + pos;
 			n &= bit_mask();
 			return m_data[n];
 		}
+		/// <summary>
+		/// 要素にアクセスする
+		/// </summary>
+		/// <param name="pos">先頭からの番地</param>
+		/// <returns>指定された要素</returns>
 		Type& operator[](size_t pos) {
 			size_t n = begin + pos;
 			n &= bit_mask();
 			return m_data[n];
 		}
 
+		/// <summary>
+		/// 先頭の要素を返す
+		/// </summary>
+		/// <returns>先頭の要素</returns>
 		const Type& front() const {
 			return m_data[begin];
 		}
+		/// <summary>
+		/// 先頭の要素を返す
+		/// </summary>
+		/// <returns>先頭の要素</returns>
 		Type& front() {
 			return m_data[begin];
 		}
 
+		/// <summary>
+		/// 終端の要素を返す
+		/// </summary>
+		/// <returns>終端の要素</returns>
 		const Type& back() const {
 			return m_data[(end - 1) & bit_mask()];
 		}
+		/// <summary>
+		/// 終端の要素を返す
+		/// </summary>
+		/// <returns>終端の要素</returns>
 		Type& back() {
 			return m_data[(end - 1) & bit_mask()];
 		}
 
+		/// <summary>
+		/// 要素を削除する
+		/// </summary>
 		void clear() noexcept {
 			begin = 0;
 			end = 0;
 			element = 0;
+			m_data.swap(ContainerType());
 		}
 
+		/// <summary>
+		/// 別のDequeと交換する
+		/// </summary>
+		/// <param name="other">Deque</param>
 		void swap(FixedDeque& other) noexcept(_Is_nothrow_swappable<Type>::value)
 		{
 			m_data.swap(other.m_data);
--- CppLib/main.cpp
+++ CppLib/main.cpp
@@ -2,34 +2,13 @@
 #include "Basic.hpp"
 #include "Queue.hpp"
 #include "FixedDeque.hpp"
+#include "Stopwatch.hpp"
+
+#include <deque>
 
 using namespace std;
 
 int main() {
-
-	alib::FixedDeque<int, 16> deque;
-	alib::FixedDeque<int, 16> deque2;
-	alib::FixedDeque<int, 8> deque3;
-
-	cout << "size:" << deque.size() << endl;
-	cout << "element:" << deque.element_num() << endl;
-
-	deque.push_back(1);
-	deque.push_back(2);
-	deque.push_back(3);
-
-	deque.push_front(-1);
-	deque.push_front(-2);
-	deque.push_front(-3);
-
-	while (!deque.empty()) {
-		cout << deque.front() << endl;
-		deque.pop_front();
-	}
-	while (!deque2.empty()) {
-		cout << deque2.front() << endl;
-		deque2.pop_front();
-	}
 
 	return 0;
 }
