593508204da0ff70897d34f9877af3de02c321d6
浦上拓人 <ao14159265@gmail.com>
2017-08-15 17:12:30 +0900

    FixedQueue作成

--- CppLib/FixedQueue.hpp
+++ CppLib/FixedQueue.hpp
@@ -5,51 +5,63 @@
 namespace alib {
 
 	template<typename Type, size_t Size>
-	class Queue {
+	class FixedQueue {
+	private:
+
+		FixedDeque<Type, Size> que;
+
 	public:
 
-		Queue() = default;
-		Queue(const Queue& other) {
-
-		}
-		Queue(Queue&& other) {
-
+		FixedQueue() = default;
+		FixedQueue(const FixedQueue& other) = default;
+		FixedQueue(FixedQueue&& other) {
+			que = std::move(other);
 		}
 
-		const bool empty() const {
-			return start == end;
-		}
-
-		constexpr int size() const noexcept {
-			return containerAllocator(Size);
-		}
-
-		const Type& front() const {
-
-		}
+		FixedQueue& operator=(const FixedQueue& other) = default;
+		FixedQueue& operator=(FixedQueue&& other) = default;
 
 		void push(const Type& v) {
-
+			que.push_back(v);
 		}
 		void push(Type&& v) {
-
-		}
-
-		void emplace(Type&& v) {
-
+			que.push_back(v);
 		}
 
 		void pop() {
-
+			que.pop_front();
 		}
 
-		void swap(Queue& other) {
-
+		constexpr size_t size() const noexcept {
+			return que.size();
 		}
 
-	private:
+		size_t element_num() const noexcept {
+			return que.element_num();
+		}
 
-		FixedDeque<Type, Size> deque;
+		bool empty() const noexcept {
+			return que.empty();
+		}
+
+		bool full() const noexcept {
+			return que.full();
+		}
+
+		const Type& front() const {
+			return que.front();
+		}
+		Type& front() {
+			return que.front();
+		}
+
+		void clear() noexcept {
+			que.clear();
+		}
+
+		void swap(FixedQueue& other) noexcept(_Is_nothrow_swappable<Type>::value) {
+			que.swap(other);
+		}
 
 	};
 
--- CppLib/main.cpp
+++ CppLib/main.cpp
@@ -1,14 +1,43 @@
 #include <iostream>
-#include "Basic.hpp"
-#include "FixedDeque.hpp"
 #include "FixedQueue.hpp"
 #include "Stopwatch.hpp"
-
-#include <deque>
 
 using namespace std;
 
 int main() {
+
+	alib::FixedQueue<int, 4> que1;
+	alib::FixedQueue<int, 4> que2;
+
+	cout << "size1:" << que1.size() << endl;
+	cout << "size2:" << que2.size() << endl;
+
+	que1.push(1);
+	que1.push(2);
+	que1.push(3);
+	que1.push(4);
+	que1.push(5);
+
+	que2.push(1);
+	que2.push(2);
+	que2.push(3);
+
+	cout << "size1:" << que1.element_num() << endl;
+	cout << "size2:" << que2.element_num() << endl;
+
+	cout << "que1" << endl;
+	while (!que1.empty())
+	{
+		cout << que1.front() << endl;
+		que1.pop();
+	}
+
+	cout << "que2" << endl;
+	while (!que2.empty())
+	{
+		cout << que2.front() << endl;
+		que2.pop();
+	}
 
 	return 0;
 }
