57b176ab6444c3cea46f40bf6ceee23e1168c222
ao1415 <ao14159265@gmail.com>
2020-11-28 21:33:00 +0900

    固定長優先度付きキュー

--- CppLib/FiexdPriorityQueue.hpp
+++ CppLib/FiexdPriorityQueue.hpp
@@ -1,22 +1,75 @@
 #pragma once
 
-#include <queue>
-#include "MemoryPool.hpp"
+#include <array>
+#include <algorithm>
 
-namespace alib {
+#include "Basic.hpp"
 
-	template<class Type, size_t Size, class _Pr = std::less<typename std::vector<Type>::value_type>>
-	class FiexdPriorityQueue {
+namespace alib
+{
+
+	template <class Type, size_t Size, class Compare = std::less<typename Type>>
+	class FiexdPriorityQueue
+	{
+	public:
+		using value_type = Type;
+		using reference = Type &;
+		using const_reference = const Type &;
+		using value_compare = Compare;
+
+		using Sequence = std::array<Type, containerAllocator(Size)>;
+
 	private:
+		Sequence c;
+		Compare comp;
 
-		std::priority_queue<Type, std::vector<Type>, _Pr> m_queue;
-
-		MemoryPool pool<Type, Size> m_pool;
+		size_t end = 0;
 
 	public:
+		FiexdPriorityQueue() : c(), comp() {}
 
+		explicit FiexdPriorityQueue(const Compare &__x, size_t __e, const Sequence &__s) : c(__s), end(__e), comp(__x)
+		{
+			std::make_heap(c.begin(), c.begin() + end, comp);
+		}
 
+		explicit FiexdPriorityQueue(const Compare &__x, size_t __e = 0, Sequence &&__s = Sequence()) : c(std::move(__s)), end(__e), comp(__x)
+		{
+			std::make_heap(c.begin(), c.begin() + end, comp);
+		}
 
+		bool empty() const { return (end == 0); }
+		size_t size() const { return end; }
+
+		const_reference top() const { return c.front(); }
+
+		void push(const value_type &__x)
+		{
+			c[end] = __x;
+			end++;
+			std::push_heap(c.begin(), c.begin() + end, comp);
+		}
+
+		void push(value_type &&__x)
+		{
+			c[end] = std::move(__x);
+			end++;
+			std::push_heap(c.begin(), c.begin() + end, comp);
+		}
+
+		template <typename... _Args>
+		void emplace(_Args &&... __args)
+		{
+			c[end] = std::move(Type(std::forward<_Args>(__args)...));
+			end++;
+			std::push_heap(c.begin(), c.begin() + end, comp);
+		}
+
+		void pop()
+		{
+			std::pop_heap(c.begin(), c.begin() + end, comp);
+			end--;
+		}
 	};
 
-}
+} // namespace alib
--- CppLib/main.cpp
+++ CppLib/main.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 #include <numeric>
+#include <queue>
 
 #include "Stopwatch.hpp"
 
-#include "Random.hpp"
+#include "FiexdPriorityQueue.hpp"
 
 using namespace std;
 
@@ -12,44 +13,25 @@ int main() {
 	using namespace alib;
 
 	Stopwatch sw;
-
-	XoShiro128 xoshiro;
-
-	double minVal = numeric_limits<double>::max();
-	double maxVal = numeric_limits<double>::min();
-	for (int i = 0; i < 100000; i++)
-	{
-		const double n = xoshiro.nextDouble();
-		minVal = min(minVal, n);
-		maxVal = max(maxVal, n);
-	}
-	cout << minVal << endl;
-	cout << maxVal << endl;
+	FiexdPriorityQueue<int, 2 << 16> que;
 
 	sw.start();
 
-	XoShiro128::value_type val = 0;
-	for (int i = 0; i < 1000000; i++)
+	que.push(100000);
+	forange(i, 100000)
 	{
-		val ^= xoshiro.next();
+		que.push(i);
 	}
 
-	sw.stop();
-
-	cout << val << endl;
-
-	cout << sw.toString_ms() << endl;
-
-	sw.start();
-	double val2 = 0;
-	for (int i = 0; i < 1000000; i++)
+	forange(i, 100000)
 	{
-		val2 += xoshiro.nextDouble();
+		que.pop();
 	}
+	
+	cout << que.top() << endl;
+
 
 	sw.stop();
-
-	cout << val2 << endl;
 
 	cout << sw.toString_ms() << endl;
 
