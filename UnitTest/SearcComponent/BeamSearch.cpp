c737fd2426276b58ffca3c5c48ecc99ce58c738a
ao1415 <ao14159265@gmail.com>
2022-04-17 17:35:10 +0900

    テストケース追加

--- UnitTest/SearcComponent/BeamSearch.cpp
+++ UnitTest/SearcComponent/BeamSearch.cpp
@@ -17,7 +17,6 @@ namespace SearcComponent::BeamSearchTest {
 		};
 
 		using Method = alib::Search::DefaultSearchMethod<Arg, double, size_t>;
-		//using Method = alib::Search::DefaultSearchMethod<Arg>;
 		using Config = alib::Search::ConfigTemplate<3, 10, 2000 * 1000, Method>;
 
 		class Beam : public alib::Search::BeamSearchTemplate<Config, Beam> {
@@ -35,7 +34,6 @@ namespace SearcComponent::BeamSearchTest {
 			void init(const SearchMethod&) noexcept override {
 				forange_type(int, i, 10) {
 					nextSearch(i, i, i);
-					//nextSearch(i, i);
 				}
 			}
 
@@ -44,7 +42,6 @@ namespace SearcComponent::BeamSearchTest {
 				list[depth] = arg.argument.value;
 				forange_type(int, i, 10) {
 					nextSearch(arg.score + i, arg.hash * 10 + i, i);
-					//nextSearch(arg.score + i, i);
 				}
 				int a = 0;
 			}
@@ -54,6 +51,58 @@ namespace SearcComponent::BeamSearchTest {
 		Beam beam;
 		beam.start({});
 		//beam.start({ 0, Arg() });
+		const auto result = beam.getResultList();
+
+		ASSERT_EQ(9, result[0].value);
+		ASSERT_EQ(9, result[1].value);
+		ASSERT_EQ(9, result[2].value);
+	}
+
+	/**
+	 * @brief 単純機能テスト
+	*/
+	TEST(SearcComponent_BeamSearch, BeamSearch_FuncTest02) {
+
+		struct Arg {
+			Arg() {}
+			Arg(const int v) : value(v) {}
+			int value{};
+		};
+
+		using Method = alib::Search::DefaultSearchMethod<Arg>;
+		using Config = alib::Search::ConfigTemplate<3, 10, 2000 * 1000, Method>;
+
+		class Beam : public alib::Search::BeamSearchTemplate<Config, Beam> {
+		public:
+			using base = alib::Search::BeamSearchTemplate<Config, Beam>;
+			using SearchMethod = base::SearchMethod;
+			using Reference = alib::Search::MemoType<Beam>;
+
+		private:
+
+			Reference::Array<Reference::Value<int>, 3> list;
+
+		public:
+
+			void init(const SearchMethod&) noexcept override {
+				forange_type(int, i, 10) {
+					nextSearch(i, i);
+				}
+			}
+
+			void search(const SearchMethod& arg) noexcept override {
+				const auto depth = getDepth();
+				list[depth] = arg.argument.value;
+				forange_type(int, i, 10) {
+					nextSearch(arg.score + i, i);
+				}
+				int a = 0;
+			}
+
+		};
+
+		Beam beam;
+		beam.start({});
 		const auto result = beam.getResultList();
 
 		ASSERT_EQ(9, result[0].value);
